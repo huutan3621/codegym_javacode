359c4835a1456efabc77e6a391ace166be933fd5
itstanryo <huutan3621@gmail.com>
2021-05-16 22:46:09 +0700

    commit

--- operators/ptbac1.js
+++ operators/ptbac1.js
@@ -2,12 +2,16 @@ let a = +prompt('Nhap vao so a:');
 let b = +prompt('Nhap vao so b:');
 
 if(a==0){
-    if(b==0){
+    if( b == 0 ){
         document.write("Phuong trinh co vo so nghiem.");
-    } else{
+    } 
+    else
+    {
         document.write("Phuong trinh vo nghiem.");
     }
-}else{
-    x=-b/a;
+}
+else
+{
+    x = - b / a;
     document.write("Phuong trinh co nghiem: " +x);
 }
---
+++ operators/usdtovnd.js
@@ -0,0 +1,5 @@
+let USD = +prompt('Nhap vao so tien USD:');
+
+VND = USD * 23000;
+
+document.write("So tien sau khi chuyen doi: " +VND);

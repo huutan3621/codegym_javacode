5af3e4c3f2701fbdbcebcff72f6c5e6cf1cd510a
itstanryo <huutan3621@gmail.com>
2021-05-18 19:48:21 +0700

    commit

--- lab08/chuvidt.js
+++ lab08/chuvidt.js
@@ -1,6 +1,5 @@
-let a = +prompt('Nhap vao chieu dai:');
-let b = +prompt('Nhap vao chieu rong:');
-s = ( a + b ) * 2;
-p = a * b;
-document.writeln('Chu vi: ' +s);
-document.write('Dien tich ' +p);
+let a = +prompt('Nhap vao ban kinh:');
+s = a * a * 3.14;
+p = 2 * 3.14 * a;
+document.writeln('Chu vi: ' +p);
+document.write('Dien tich ' +s);

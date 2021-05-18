0cfc7df8d91db4930e7b331f30604865efe514f8
itstanryo <huutan3621@gmail.com>
2021-05-18 20:09:53 +0700

    commit

---
+++ lab08/letter.js
@@ -0,0 +1,9 @@
+let tname = prompt("Nhap ho ten nguoi nhan:");
+let date = +prompt("Nhap ngay gui: ");
+let month = +prompt("Nhap thang:");
+let year = +prompt("Nhap nam:");
+let placesent = prompt("Nhap ten dia diem: ");
+
+document.writeln(tname," than men ","<p></p>");
+document.writeln("Noi dung buc thu o day","<p></p>");
+document.write(placesent, " ngay ",date, " thang ", month, " nam ", year);
